04b48e164c403b7fb9fd9cbfdf04fa382b8ef1b0
Anjali Verma <anjaliverma19171@gmail.com>
2026-05-19 15:43:58 +0530

    successful

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'creating_world' });
+  res.render('index', { title: 'cicd deployment through github actions successful' });
 });
 
 module.exports = router;
