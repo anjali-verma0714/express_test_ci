fb42d3241ee8f27997d3b4d4dc9116c1a9dc67a3
Anjali Verma <anjaliverma19171@gmail.com>
2026-05-20 12:00:56 +0530

    add deploy workflow

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'cicd deployment through github actions successful' });
+  res.render('index', { title: 'cicd deployment through github actions successful❤️❤️❤️' });
 });
 
 module.exports = router;
