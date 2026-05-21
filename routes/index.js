6ca603b7c375fef557ebcf1c2dfbb0e3e916303a
Anjali Verma <anjaliverma19171@gmail.com>
2026-05-21 11:17:35 +0530

    name added in the title

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'CI/CD DevOps Project 🚀',
+    title: 'CI/CD DevOps Project  by Anjali 🚀',
 
     projectInfo: {
       description: 'Node.js + Express application deployed using CI/CD pipeline',
